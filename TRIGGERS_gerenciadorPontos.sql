b8f5cdca49d2abdf01c806108721dd422a74bcf0
Julio Cesar Moreira <juliopaesmoreira@hotmail.com>
2021-03-18 21:10:50 -0300

    feat: Inserindo triggers, procedures e funções no banco

--- TRIGGERS_gerenciadorPontos.sql
+++ TRIGGERS_gerenciadorPontos.sql
@@ -1,11 +1,12 @@
-USE gerenciador_pontos;
--- ------------- ENTERPRISES ------------ 
-DROP TRIGGER IF EXISTS trg_ins_enterprise;
+USE gerenciadorPontos;
+
 DELIMITER $$
 CREATE TRIGGER trg_ins_enterprise AFTER INSERT
 ON enterprises
 FOR EACH ROW
 BEGIN
+	UPDATE new.enterprise SET new.created_at = SYSDATE();
+    UPDATE new.enterprise SET new.updated_at = SYSDATE();
     CALL prc_valida_email (new.email);
 END $$
 DELIMITER ;
@@ -32,6 +33,8 @@ CREATE TRIGGER trg_ins_members AFTER INSERT
 ON members
 FOR EACH ROW
 BEGIN
+	UPDATE new.members SET new.created_at = SYSDATE();
+    UPDATE new.members SET new.updated_at = SYSDATE();
     CALL prc_valida_email (new.email);
 END $$
 DELIMITER ;
