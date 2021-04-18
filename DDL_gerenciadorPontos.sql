cdf56ed6968775a991690ebeedc8d526f7ce9369
Julio Cesar <juliopaesmoreira@hotmail.com>
2021-04-18 14:18:25 -0300

    Adicionando SEASONS e BADGES ao DDL e melhorando padronização do código

--- DDL_gerenciadorPontos.sql
+++ DDL_gerenciadorPontos.sql
@@ -1,7 +1,7 @@
 /* modeloLogico_gerenciadorPontos: */
-
+use gerenciador_pontos;
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(255) UNIQUE,
     email_verified_at TIMESTAMP,
@@ -13,7 +13,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE members (
-    member_id INTEGER PRIMARY KEY,
+    member_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50),
     email VARCHAR(50) UNIQUE,
     active BOOLEAN,
@@ -25,7 +25,7 @@ CREATE TABLE members (
 );
 
 CREATE TABLE categories (
-    category_id INTEGER PRIMARY KEY,
+    category_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(35),
     description VARCHAR(80),
     fk_enterprises_enterprise_id INTEGER,
@@ -35,7 +35,7 @@ CREATE TABLE categories (
 );
 
 CREATE TABLE rules (
-    rule_id INTEGER PRIMARY KEY,
+    rule_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30),
     point INTEGER,
     fk_enterprises_enterprise_id INTEGER,
@@ -45,7 +45,7 @@ CREATE TABLE rules (
 );
 
 CREATE TABLE events (
-    event_id INTEGER PRIMARY KEY,
+    event_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50),
     description VARCHAR(255),
     started_at DATETIME,
@@ -57,21 +57,21 @@ CREATE TABLE events (
     deleted_at TIMESTAMP
 );
 
-CREATE TABLE rules_categories_belongsToMany (
+CREATE TABLE rules_categories (
     rule_category_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     active_ BOOLEAN,
     fk_rules_rule_id INTEGER,
     fk_categories_category_id INTEGER
 );
 
-CREATE TABLE events_members_belongsToMany (
+CREATE TABLE events_members (
     event_member_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     fk_members_member_id INTEGER,
     fk_events_event_id INTEGER
 );
 
 CREATE TABLE enterprises (
-    enterprise_id INTEGER PRIMARY KEY,
+    enterprise_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(255),
     created_at TIMESTAMP,
@@ -80,7 +80,7 @@ CREATE TABLE enterprises (
 );
 
 CREATE TABLE teams (
-    team_id INTEGER PRIMARY KEY,
+    team_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30),
     fk_enterprises_enterprise_id INTEGER,
     created_at TIMESTAMP,
@@ -88,67 +88,116 @@ CREATE TABLE teams (
     deleted_at TIMESTAMP
 );
 
-CREATE TABLE teams_members_belongsToMany (
-    team_member_id INTEGER PRIMARY KEY,
+CREATE TABLE teams_members (
+    team_member_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     fk_teams_team_id INTEGER,
     fk_members_member_id INTEGER
 );
+
+CREATE TABLE seasons (
+	season_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(80),
+    description VARCHAR(255),
+    started_at TIMESTAMP,
+    ended_at TIMESTAMP,
+    fk_enterprises_enterprise_id INTEGER
+);
+
+CREATE TABLE badges (
+	badge_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    trophy BOOLEAN,
+    name VARCHAR(80),
+    description VARCHAR(255),
+    fk_enterprises_enterprise_id INTEGER,
+    fk_seasons_season_id INTEGER
+);
+
+CREATE TABLE badges_members (
+	badges_members_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    fk_badges_members_badge_id INTEGER,
+    fk_badges_members_member_id INTEGER
+);
  
-ALTER TABLE users ADD CONSTRAINT FK_users_2
+ALTER TABLE seasons ADD CONSTRAINT FK_season_enterprise
+	FOREIGN KEY (fk_enterprises_enterprise_id)
+    REFERENCES enterprises (enterprise_id)
+    ON DELETE RESTRICT;
+    
+ALTER TABLE badges ADD CONSTRAINT FK_badge_enterprise
+	FOREIGN KEY (fk_enterprises_enterprise_id)
+    REFERENCES enterprises (enterprise_id)
+    ON DELETE RESTRICT;
+    
+ALTER TABLE badges ADD CONSTRAINT FK_badge_season
+	FOREIGN KEY (fk_seasons_season_id)
+    REFERENCES seasons (season_id)
+    ON DELETE RESTRICT;
+    
+ALTER TABLE badges_members ADD CONSTRAINT FK_badge_member_badge
+	FOREIGN KEY (fk_badges_members_badge_id)
+    REFERENCES badges (badge_id)
+    ON DELETE RESTRICT;
+
+ALTER TABLE badges_members ADD CONSTRAINT FK_badge_member_member
+	FOREIGN KEY (fk_badges_members_member_id)
+    REFERENCES members (member_id)
+    ON DELETE RESTRICT;
+
+ALTER TABLE users ADD CONSTRAINT FK_users_enterprise
     FOREIGN KEY (fk_enterprises_enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE RESTRICT;
  
-ALTER TABLE members ADD CONSTRAINT FK_members_2
+ALTER TABLE members ADD CONSTRAINT FK_members_enterprise
     FOREIGN KEY (fk_enterprises_enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE RESTRICT;
  
-ALTER TABLE categories ADD CONSTRAINT FK_categories_2
+ALTER TABLE categories ADD CONSTRAINT FK_categories_enterprise
     FOREIGN KEY (fk_enterprises_enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE CASCADE;
  
-ALTER TABLE rules ADD CONSTRAINT FK_rules_2
+ALTER TABLE rules ADD CONSTRAINT FK_rules_enterprise
     FOREIGN KEY (fk_enterprises_enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE CASCADE;
  
-ALTER TABLE events ADD CONSTRAINT FK_events_2
+ALTER TABLE events ADD CONSTRAINT FK_events_category
     FOREIGN KEY (fk_categories_category_id)
     REFERENCES categories (category_id)
     ON DELETE CASCADE;
  
-ALTER TABLE events ADD CONSTRAINT FK_events_3
+ALTER TABLE events ADD CONSTRAINT FK_events_enterprise
     FOREIGN KEY (fk_enterprises_enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE CASCADE;
  
-ALTER TABLE rules_categories_belongsToMany ADD CONSTRAINT FK_rules_categories_belongsToMany_2
+ALTER TABLE rules_categories ADD CONSTRAINT FK_rules_categories_rule
     FOREIGN KEY (fk_rules_rule_id)
     REFERENCES rules (rule_id);
  
-ALTER TABLE rules_categories_belongsToMany ADD CONSTRAINT FK_rules_categories_belongsToMany_3
+ALTER TABLE rules_categories ADD CONSTRAINT FK_rules_categories_category
     FOREIGN KEY (fk_categories_category_id)
     REFERENCES categories (category_id);
  
-ALTER TABLE events_members_belongsToMany ADD CONSTRAINT FK_events_members_belongsToMany_2
+ALTER TABLE events_members ADD CONSTRAINT FK_events_members_member
     FOREIGN KEY (fk_members_member_id)
     REFERENCES members (member_id);
  
-ALTER TABLE events_members_belongsToMany ADD CONSTRAINT FK_events_members_belongsToMany_3
+ALTER TABLE events_members ADD CONSTRAINT FK_events_members_event
     FOREIGN KEY (fk_events_event_id)
     REFERENCES events (event_id);
  
-ALTER TABLE teams ADD CONSTRAINT FK_teams_2
+ALTER TABLE teams ADD CONSTRAINT FK_teams_enterprise
     FOREIGN KEY (fk_enterprises_enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE CASCADE;
  
-ALTER TABLE teams_members_belongsToMany ADD CONSTRAINT FK_teams_members_belongsToMany_2
+ALTER TABLE teams_members ADD CONSTRAINT FK_teams_members_team
     FOREIGN KEY (fk_teams_team_id)
     REFERENCES teams (team_id);
  
-ALTER TABLE teams_members_belongsToMany ADD CONSTRAINT FK_teams_members_belongsToMany_3
+ALTER TABLE teams_members ADD CONSTRAINT FK_teams_members_member
     FOREIGN KEY (fk_members_member_id)
     REFERENCES members (member_id);
