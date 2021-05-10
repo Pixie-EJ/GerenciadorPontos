add4df599394ca3958d7fabb6416fb32b6d41365
Julio Cesar <juliopaesmoreira@hotmail.com>
2021-05-10 20:40:21 -0300

    melhorias no DDL e inicio da função de valida membro em evento

--- DDL_gerenciadorPontos.sql
+++ DDL_gerenciadorPontos.sql
@@ -6,10 +6,10 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE,
     email_verified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     password VARCHAR(255),
-    fk_enterprises_enterprise_id INTEGER,
+    enterprise_id INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP NULL DEFAULT NULL
 );
 
 CREATE TABLE members (
@@ -18,30 +18,30 @@ CREATE TABLE members (
     email VARCHAR(50) UNIQUE,
     active BOOLEAN,
     role VARCHAR(80),
-    fk_enterprises_enterprise_id INTEGER,
+    enterprise_id INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP NULL DEFAULT NULL
 );
 
 CREATE TABLE categories (
     category_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(35),
     description VARCHAR(80),
-    fk_enterprises_enterprise_id INTEGER,
+    enterprise_id INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP NULL DEFAULT NULL
 );
 
 CREATE TABLE rules (
     rule_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30),
     point INTEGER,
-    fk_enterprises_enterprise_id INTEGER,
+    enterprise_id INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP NULL DEFAULT NULL
 );
 
 CREATE TABLE events (
@@ -50,25 +50,25 @@ CREATE TABLE events (
     description VARCHAR(255),
     started_at DATETIME,
     ended_at DATETIME,
-    fk_categories_category_id INTEGER,
-    fk_enterprises_enterprise_id INTEGER,
-    fk_seasons_season_id INTEGER,
+    category_id INTEGER,
+    enterprise_id INTEGER,
+    season_id INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP NULL DEFAULT NULL
 );
 
 CREATE TABLE rules_categories (
     rule_category_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     active_ BOOLEAN,
-    fk_rules_rule_id INTEGER,
-    fk_categories_category_id INTEGER
+    rule_id INTEGER,
+    category_id INTEGER
 );
 
 CREATE TABLE events_members (
     event_member_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    fk_members_member_id INTEGER,
-    fk_events_event_id INTEGER
+    member_id INTEGER,
+    event_id INTEGER
 );
 
 CREATE TABLE enterprises (
@@ -77,22 +77,22 @@ CREATE TABLE enterprises (
     email VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP NULL DEFAULT NULL
 );
 
 CREATE TABLE teams (
     team_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30),
-    fk_enterprises_enterprise_id INTEGER,
+    enterprise_id INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP NULL DEFAULT NULL
 );
 
 CREATE TABLE teams_members (
     team_member_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    fk_teams_team_id INTEGER,
-    fk_members_member_id INTEGER
+    team_id INTEGER,
+    member_id INTEGER
 );
 
 CREATE TABLE seasons (
@@ -101,10 +101,10 @@ CREATE TABLE seasons (
     description VARCHAR(255),
     started_at TIMESTAMP,
     ended_at TIMESTAMP,
-    fk_enterprises_enterprise_id INTEGER,
+    enterprise_id INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP NULL DEFAULT NULL
 );
 
 CREATE TABLE badges (
@@ -112,104 +112,104 @@ CREATE TABLE badges (
     trophy BOOLEAN,
     name VARCHAR(80),
     description VARCHAR(255),
-    fk_enterprises_enterprise_id INTEGER,
-    fk_seasons_season_id INTEGER,
+    enterprise_id INTEGER,
+    season_id INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP NULL DEFAULT NULL
 );
 
 CREATE TABLE badges_members (
 	badges_members_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    fk_badges_members_badge_id INTEGER,
-    fk_badges_members_member_id INTEGER
+    badge_id INTEGER,
+    member_id INTEGER
 );
  
 ALTER TABLE seasons ADD CONSTRAINT FK_season_enterprise
-	FOREIGN KEY (fk_enterprises_enterprise_id)
+	FOREIGN KEY (enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE RESTRICT;
     
 ALTER TABLE badges ADD CONSTRAINT FK_badge_enterprise
-	FOREIGN KEY (fk_enterprises_enterprise_id)
+	FOREIGN KEY (enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE RESTRICT;
     
 ALTER TABLE badges ADD CONSTRAINT FK_badge_season
-	FOREIGN KEY (fk_seasons_season_id)
+	FOREIGN KEY (season_id)
     REFERENCES seasons (season_id)
     ON DELETE RESTRICT;
     
 ALTER TABLE badges_members ADD CONSTRAINT FK_badge_member_badge
-	FOREIGN KEY (fk_badges_members_badge_id)
+	FOREIGN KEY (badge_id)
     REFERENCES badges (badge_id)
     ON DELETE RESTRICT;
 
 ALTER TABLE badges_members ADD CONSTRAINT FK_badge_member_member
-	FOREIGN KEY (fk_badges_members_member_id)
+	FOREIGN KEY (member_id)
     REFERENCES members (member_id)
     ON DELETE RESTRICT;
 
 ALTER TABLE users ADD CONSTRAINT FK_users_enterprise
-    FOREIGN KEY (fk_enterprises_enterprise_id)
+    FOREIGN KEY (enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE RESTRICT;
  
 ALTER TABLE members ADD CONSTRAINT FK_members_enterprise
-    FOREIGN KEY (fk_enterprises_enterprise_id)
+    FOREIGN KEY (enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE RESTRICT;
  
 ALTER TABLE categories ADD CONSTRAINT FK_categories_enterprise
-    FOREIGN KEY (fk_enterprises_enterprise_id)
+    FOREIGN KEY (enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE CASCADE;
  
 ALTER TABLE rules ADD CONSTRAINT FK_rules_enterprise
-    FOREIGN KEY (fk_enterprises_enterprise_id)
+    FOREIGN KEY (enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE CASCADE;
  
 ALTER TABLE events ADD CONSTRAINT FK_events_category
-    FOREIGN KEY (fk_categories_category_id)
+    FOREIGN KEY (category_id)
     REFERENCES categories (category_id)
     ON DELETE CASCADE;
  
 ALTER TABLE events ADD CONSTRAINT FK_events_enterprise
-    FOREIGN KEY (fk_enterprises_enterprise_id)
+    FOREIGN KEY (enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE CASCADE;
  
 ALTER TABLE events ADD CONSTRAINT FK_events_season
-    FOREIGN KEY (fk_seasons_season_id)
+    FOREIGN KEY (season_id)
     REFERENCES seasons (season_id)
     ON DELETE CASCADE; 
  
 ALTER TABLE rules_categories ADD CONSTRAINT FK_rules_categories_rule
-    FOREIGN KEY (fk_rules_rule_id)
+    FOREIGN KEY (rule_id)
     REFERENCES rules (rule_id);
  
 ALTER TABLE rules_categories ADD CONSTRAINT FK_rules_categories_category
-    FOREIGN KEY (fk_categories_category_id)
+    FOREIGN KEY (category_id)
     REFERENCES categories (category_id);
  
 ALTER TABLE events_members ADD CONSTRAINT FK_events_members_member
-    FOREIGN KEY (fk_members_member_id)
+    FOREIGN KEY (member_id)
     REFERENCES members (member_id);
  
 ALTER TABLE events_members ADD CONSTRAINT FK_events_members_event
-    FOREIGN KEY (fk_events_event_id)
+    FOREIGN KEY (event_id)
     REFERENCES events (event_id);
  
 ALTER TABLE teams ADD CONSTRAINT FK_teams_enterprise
-    FOREIGN KEY (fk_enterprises_enterprise_id)
+    FOREIGN KEY (enterprise_id)
     REFERENCES enterprises (enterprise_id)
     ON DELETE CASCADE;
  
 ALTER TABLE teams_members ADD CONSTRAINT FK_teams_members_team
-    FOREIGN KEY (fk_teams_team_id)
+    FOREIGN KEY (team_id)
     REFERENCES teams (team_id);
  
 ALTER TABLE teams_members ADD CONSTRAINT FK_teams_members_member
-    FOREIGN KEY (fk_members_member_id)
+    FOREIGN KEY (member_id)
     REFERENCES members (member_id);
--- PROCEDURES_gerenciadorPontos.sql
+++ PROCEDURES_gerenciadorPontos.sql
@@ -39,9 +39,9 @@ DELIMITER $$
 			SELECT em.event_member_id, ev.started_at, ev.ended_at, ev.event_id
 			FROM events ev
             JOIN events_members em
-            ON ev.event_id = em.fk_events_event_id
+            ON ev.event_id = em.event_id
 			JOIN seasons s
-			ON ev.fk_seasons_season_id = s.season_id
+			ON ev.season_id = s.season_id
 			WHERE ev.started_at BETWEEN s.started_at AND s.ended_at
 			AND ev.ended_at BETWEEN s.started_at AND s.ended_at;
             OPEN c_events;
@@ -50,9 +50,9 @@ DELIMITER $$
 			INTO how_many_events
             FROM events ev
             JOIN events_members em
-            ON ev.event_id = em.fk_events_event_id
+            ON ev.event_id = em.event_id
 			JOIN seasons s
-			ON ev.fk_seasons_season_id = s.season_id
+			ON ev.season_id = s.season_id
 			WHERE ev.started_at BETWEEN s.started_at AND s.ended_at
 			AND ev.ended_at BETWEEN s.started_at AND s.ended_at;
 		
@@ -98,10 +98,10 @@ DELIMITER ;*/
 
 DROP PROCEDURE IF EXISTS prc_add_badges;
 DELIMITER $$
-	CREATE PROCEDURE `prc_add_badges`(p_trophy BOOLEAN, p_name VARCHAR(50), p_description VARCHAR(255), p_enterprise_id INTEGER, p_season_id INTEGER)
+	CREATE PROCEDURE `prc_add_badges`(p_trophy BOOLEAN, p_name VARCHAR(50), p_role VARCHAR(80), p_enterprise_id INTEGER, p_season_id INTEGER)
 	BEGIN
-		INSERT INTO badges(trophy, name, description, fk_enterprises_enterprise_id, fk_seasons_season_id) 
-        VALUES (p_trophy, p_name, p_description, p_enterprise_id, p_season_id);
+		INSERT INTO badges(trophy, name, role, enterprise_id, season_id) 
+        VALUES (p_trophy, p_name, p_role, p_enterprise_id, p_season_id);
         COMMIT;
 	END $$
 DELIMITER ;
@@ -110,7 +110,7 @@ DROP PROCEDURE IF EXISTS prc_add_badges_members;
 DELIMITER $$
 	CREATE PROCEDURE `prc_add_badges_members`(p_badge_id INTEGER, p_member_id INTEGER)
 	BEGIN
-		INSERT INTO badges_members(fk_badges_members_badge_id, fk_badges_members_member_id)
+		INSERT INTO badges_members(badge_id, member_id)
         VALUES (p_badge_id, p_member_id);
         COMMIT;
 	END $$
@@ -120,7 +120,7 @@ DROP PROCEDURE IF EXISTS prc_add_category;
 DELIMITER $$
 	CREATE PROCEDURE `prc_add_category`(p_name VARCHAR(50), p_description VARCHAR(300), p_interprise_id INTEGER)
     BEGIN	
-        INSERT INTO categories(name, description, fk_enterprises_enterprise_id) 
+        INSERT INTO categories(name, description, enterprise_id) 
         VALUES (p_name, p_description, p_interprise_id);
         COMMIT;
     END $$
@@ -140,7 +140,7 @@ DROP PROCEDURE IF EXISTS prc_add_event;
 DELIMITER $$
 	CREATE PROCEDURE `prc_add_event`(p_name VARCHAR(50), p_description VARCHAR(300), p_started_at TIMESTAMP, p_ended_at TIMESTAMP, p_category_id INTEGER, p_interprise_id INTEGER, p_season_id INTEGER)
     BEGIN	
-        INSERT INTO events(name, description, started_at, ended_at, fk_categories_category_id, fk_enterprises_enterprise_id, fk_seasons_season_id) 
+        INSERT INTO events(name, description, started_at, ended_at, category_id, enterprise_id, season_id) 
         VALUES (p_name, p_description, p_started_at, p_ended_at, p_category_id, p_interprise_id, p_season_id);
         COMMIT;
     END $$
@@ -150,7 +150,7 @@ DROP PROCEDURE IF EXISTS prc_add_member;
 DELIMITER $$
 	CREATE PROCEDURE `prc_add_member`(p_name VARCHAR(50), p_email VARCHAR(50), p_active TINYINT, p_role VARCHAR(80), p_interprise_id INTEGER)
     BEGIN	
-        INSERT INTO members(name, email, active, role, fk_enterprises_enterprise_id) 
+        INSERT INTO members(name, email, active, role, enterprise_id) 
         VALUES (p_name, p_email, p_active, p_role, p_interprise_id);
         COMMIT;
     END $$
@@ -160,7 +160,7 @@ DROP PROCEDURE IF EXISTS prc_add_events_members;
 DELIMITER $$
 	CREATE PROCEDURE `prc_add_events_members`(p_member_id INTEGER, p_event_id INTEGER)
     BEGIN	
-        INSERT INTO events_members(fk_members_member_id , fk_events_event_id) 
+        INSERT INTO events_members(member_id , event_id) 
         VALUES (p_member_id, p_event_id);
         COMMIT;
     END $$
@@ -170,18 +170,18 @@ DROP PROCEDURE IF EXISTS prc_add_rule;
 DELIMITER $$
 	CREATE PROCEDURE `prc_add_rule`(p_name VARCHAR(50), p_point INTEGER, p_interprise_id INTEGER)
     BEGIN	
-        INSERT INTO rules(name, email, fk_enterprises_enterprise_id) 
-        VALUES (p_name, p_email, p_interprise_id);
+        INSERT INTO rules(name, point, enterprise_id) 
+        VALUES (p_name, p_point, p_interprise_id);
         COMMIT;
     END $$
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS prc_add_rules_categories;
 DELIMITER $$
-	CREATE PROCEDURE `prc_add_rules_categories`(p_active VARCHAR(50), p_rule_id INTEGER, p_category_id INTEGER)
+	CREATE PROCEDURE `prc_add_rules_categories`(p_active TINYINT, p_rule_id INTEGER, p_category_id INTEGER)
     BEGIN	
-        INSERT INTO rules_categories(name, fk_rules_rule_id, fk_categories_category_id) 
-        VALUES (p_name, p_rule_id, p_category_id);
+        INSERT INTO rules_categories(active_, rule_id, category_id) 
+        VALUES (p_active, p_rule_id, p_category_id);
         COMMIT;
     END $$
 DELIMITER ;
@@ -190,7 +190,7 @@ DROP PROCEDURE IF EXISTS prc_add_seasons;
 DELIMITER $$
 	CREATE PROCEDURE `prc_add_seasons`( p_name VARCHAR(80), p_description VARCHAR(255), p_started_at TIMESTAMP, p_ended_at TIMESTAMP, p_enterprise_id INTEGER)
 	BEGIN
-		INSERT INTO seasons ( name, description, started_at, ended_at, fk_enterprises_enterprise_id)
+		INSERT INTO seasons ( name, description, started_at, ended_at, enterprise_id)
         VALUES ( p_name, p_description, p_started_at, p_ended_at, p_enterprise_id);
         COMMIT;
 	END $$
@@ -200,7 +200,7 @@ DROP PROCEDURE IF EXISTS prc_add_teams;
 DELIMITER $$
 	CREATE PROCEDURE `prc_add_teams`(p_name VARCHAR(50), p_interprise_id INTEGER)
     BEGIN	
-        INSERT INTO teams(name, fk_enterprises_enterprise_id) 
+        INSERT INTO teams(name, enterprise_id) 
         VALUES (p_name, p_interprise_id);
         COMMIT;
     END $$
@@ -210,7 +210,7 @@ DROP PROCEDURE IF EXISTS prc_add_teams_members;
 DELIMITER $$
 	CREATE PROCEDURE `prc_add_teams_members`(p_team_id INTEGER, p_member_id INTEGER)
     BEGIN	
-        INSERT INTO teams_members(fk_team_team_id, fk_members_member_id) 
+        INSERT INTO teams_members(team_id, member_id) 
         VALUES (p_team_id, p_member_id);
         COMMIT;
     END $$
@@ -218,9 +218,9 @@ DELIMITER ;
 
 DROP PROCEDURE IF EXISTS prc_add_users;
 DELIMITER $$
-	CREATE PROCEDURE `prc_add_users`(p_name_id INTEGER, p_email VARCHAR(50), p_password VARCHAR(255), p_interprise_id INTEGER)
+	CREATE PROCEDURE `prc_add_users`(p_name_id VARCHAR(255), p_email VARCHAR(255), p_password VARCHAR(255), p_interprise_id INTEGER)
     BEGIN	
-        INSERT INTO users(name, email, password, fk_enterprises_enterprise_id) 
+        INSERT INTO users(name, email, password, enterprise_id) 
         VALUES (p_name_id, p_email, p_password, p_interprise_id);
         COMMIT;
     END $$
